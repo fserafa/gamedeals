a812bb588f2e215279c598bc0452653f1b0d0e09
Felipe <felipe.seraf@gmail.comgit config --global user.name Felipegit coomit -m Firstgit config --global user.email felipe.seraf@gmail.com>
2020-06-26 15:52:19 -0300

    Updates

---
+++ precache-manifest.bbb11a189e9a97baf2cb42fdf68893a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19f7f9cdce2cf5109bb140772dde4a7",
+    "url": "/gamedeals/index.html"
+  },
+  {
+    "revision": "6d4443ea0af4530f11e9",
+    "url": "/gamedeals/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "d5c283c8e9ab61327fbb",
+    "url": "/gamedeals/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6d4443ea0af4530f11e9",
+    "url": "/gamedeals/static/js/2.0867bb30.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gamedeals/static/js/2.0867bb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c283c8e9ab61327fbb",
+    "url": "/gamedeals/static/js/main.f74f0267.chunk.js"
+  },
+  {
+    "revision": "158cdc13db431615033d",
+    "url": "/gamedeals/static/js/runtime-main.9309a223.js"
+  }
+]);
